dff3d0746967c718316c3dae97cdb2b294f2f475
vahid <vah.yosefi@gmail.com>
2022-06-11 06:44:18 +0430

    first program for merge application based postgres database

---
+++ merge_app_v1.py
@@ -0,0 +1,341 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 11:00:38 2022
+
+@author: Vahid
+for merge postgres for APP
+"""
+
+
+
+import pandas as pd
+import numpy as np
+import datetime
+import os
+import time
+import pyodbc
+from pyodbc import *
+import psycopg2
+import pandas.io.sql as psql
+from sqlalchemy import create_engine
+
+
+# start = time.time() 
+
+# df0 = pd.DataFrame()
+# total = pd.DataFrame()
+
+
+######################## read data from dabase
+connection = psycopg2.connect(user="postgres",
+                            password="12344321",
+                            host="10.32.141.17",
+                            port="5432",
+                            database="googleplay")
+cursor = connection.cursor()
+#########   googleplay
+googleplay = psql.read_sql('select * from public."googleplay_meta2022041822"', connection)
+print(len(googleplay))
+
+
+googleplay['cat'] = googleplay['cat'].str.replace('gane','game')
+googleplay['platform'] = 'googleplay'
+googleplay['creator'] = ''
+googleplay['OS'] = 'Android'
+googleplay['age range']=''
+googleplay['Cost']=''
+
+googleplay = googleplay[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+
+# googleplay.to_excel(r'D:\python\PowerBI_App_store\progress\test\googleplay1.xlsx', index=False)
+
+googleplay.dtypes
+
+
+print('googleplay:',len(googleplay))
+print('فراخونی محتوای googleplay')
+
+
+############## cafebazar
+
+
+connection = psycopg2.connect(user="postgres",
+                            password="12344321",
+                            host="10.32.141.17",
+                            port="5432",
+                            database="cafebazar")
+cursor = connection.cursor()
+
+
+cafebazar = psql.read_sql('select * from public."cafebazar_meta2022042908"', connection)
+
+cafebazar.dtypes
+print("cafebazar:" ,len(cafebazar))
+
+print('فراخونی محتوای cafebazar')
+
+cafebazar['platform'] = 'cafebazar'
+cafebazar['OS'] = 'Android'
+cafebazar['creator'] = ''
+cafebazar['Updated'] = ''
+
+cafebazar['age range']=''
+cafebazar['Cost']=''
+
+cafebazar = cafebazar[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+
+# cafebazar.to_excel(r'D:\python\PowerBI_App_store\progress\test\cafebazar1.xlsx', index=False)
+
+
+#####################################################
+
+###########  myket
+
+connection = psycopg2.connect(user="postgres",
+                            password="12344321",
+                            host="10.32.141.17",
+                            port="5432",
+                            database="Myket")
+cursor = connection.cursor()
+
+
+myket = psql.read_sql('select * from public."myket_meta2022042007"', connection)
+print('myket:',len(myket))
+print('فراخونی محتوای myket')
+# gabeh_1000 = gabeh[1:1000]
+myket['platform'] = 'myket'
+myket['OS'] = 'Android'
+myket['creator'] = myket['Creator']
+myket['Installs'] = myket['Download']
+myket['Updated'] = myket['Last Update']
+
+myket['age range']=''
+myket['Cost']=''
+
+myket.dtypes
+
+myket = myket[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+
+
+# myket.to_excel(r'D:\python\PowerBI_App_store\progress\test\myket1.xlsx', index=False)
+
+
+####################################################
+
+
+############       anardoni
+
+connection = psycopg2.connect(user="postgres",
+                            password="12344321",
+                            host="10.32.141.17",
+                            port="5432",
+                            database="anardoni")
+cursor = connection.cursor()
+
+anardoni = psql.read_sql('select * from public."anardoni_meta2022051615"', connection)
+print('anardoni:',len(anardoni))
+print('فراخونی محتوای anardoni')
+
+anardoni['Installs'] = anardoni['Download']
+anardoni['Updated'] = anardoni['Last Update']
+anardoni['creator'] = anardoni['Creator']
+
+anardoni['platform'] = 'anardoni'
+anardoni['OS'] = 'IOS'
+
+# anardoni['age range']=
+# anardoni['Cost']=
+
+anardoni['rate'] = anardoni['rate'].astype(str).astype(float)
+anardoni['ratings'] = anardoni['ratings'].astype(str).astype(int)
+anardoni['Installs'] = anardoni['Installs'].astype(str).astype(int)
+
+anardoni = anardoni[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+
+
+# anardoni.to_excel(r'D:\python\PowerBI_App_store\progress\test\anardoni1.xlsx', index=False)
+anardoni.dtypes
+
+
+################################################
+
+
+################   nassaab
+
+connection = psycopg2.connect(user="postgres",
+                            password="12344321",
+                            host="10.32.141.17",
+                            port="5432",
+                            database="nassaab")
+cursor = connection.cursor()
+
+nassaab = psql.read_sql('select * from public."nassaab_meta2022051414"', connection)
+print('nassaab:',len(nassaab))
+print('فراخونی محتوای nassaab')
+nassaab['platform'] = 'nassaab'
+
+nassaab['Installs'] = nassaab['Download']
+nassaab['Updated'] = nassaab['Last Update']
+nassaab['creator'] = nassaab['Creator']
+nassaab['age range']=''
+nassaab['Cost']=''
+nassaab['OS'] = 'IOS'
+
+nassaab.dtypes
+
+nassaab['Installs'] = nassaab['Installs'].astype(str).astype(int)
+
+nassaab = nassaab[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+# nassaab.to_excel(r'D:\python\PowerBI_App_store\progress\test\nassaab1.xlsx', index=False)
+
+print('nassaab:', len(nassaab))
+
+
+
+######################################################
+
+# ################   sibapp
+
+
+connection = psycopg2.connect(user="postgres",
+                            password="12344321",
+                            host="10.32.141.17",
+                            port="5432",
+                            database="sibapp")
+cursor = connection.cursor()
+
+sibapp = psql.read_sql('select * from public."sibapp_meta2022050213"', connection)
+print('sibapp :',len(sibapp))
+print('فراخونی محتوای sibapp')
+
+
+sibapp['platform'] = 'sibapp'
+
+sibapp['Installs'] = sibapp['Download']
+sibapp['Updated'] = ''
+sibapp['creator'] = ''
+sibapp['age range']=''
+sibapp['Cost']=''
+sibapp['OS'] = 'IOS'
+sibapp.dtypes
+
+sibapp = sibapp[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+# sibapp.to_excel(r'D:\python\PowerBI_App_store\progress\test\sibapp.xlsx', index=False)
+
+
+
+
+# #########################################################
+
+# ################  sibche
+
+
+connection = psycopg2.connect(user="postgres",
+                            password="12344321",
+                            host="10.32.141.17",
+                            port="5432",
+                            database="sibche")
+cursor = connection.cursor()
+
+sibche = psql.read_sql('select * from public."sibche_meta2022051507"', connection)
+print('sibche:',len(sibche))
+print('فراخونی محتوای sibche')
+
+sibche['platform'] = 'sibche'
+sibche['Updated'] = ''
+sibche['OS'] = 'IOS'
+sibche.dtypes
+
+# sibche['age range']=''
+sibche['Cost']=''
+
+sibche = sibche[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+
+
+
+#############################################################
+
+###############  merge
+
+app_merge = [googleplay,cafebazar,myket,anardoni,nassaab,sibapp,sibche]
+adgham_app = pd.concat(app_merge)
+
+
+adgham_app['rate'] = adgham_app['rate'].astype(str).astype(float)
+adgham_app['ratings'] = adgham_app['ratings'].astype(str).astype(int)
+adgham_app['Installs'] = adgham_app['Installs'].astype(int)
+
+
+
+adgham_app.dtypes
+
+sibche = sibche[['cat','categori_name','content_name','content_link','rate','ratings','Updated','Size','Installs','Current Version','crawling_date','age range','Cost','creator','OS','platform']]
+
+
+# adgham0['viwes'] = adgham0['viwes'].astype(float)
+# adgham0['like_count'] = adgham0['like_count'].astype(float)
+# adgham0['follower'] = adgham0['follower'].astype(float)
+
+engine = create_engine('postgresql://postgres:12344321@10.32.141.17/Vahid01',pool_size=20, max_overflow=100,)
+con=engine.connect()
+
+
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Action','اکشن')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Sports','ورزشی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Social','اجتماعی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Puzzle','معما')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Communication','ارتباطی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Art & Design','هنر و  طراحی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Watch apps','برنامه های ساعت هوشمند')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Productivity','بهره وری')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Photography','عکاسی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Video Players & Editors','ویرایش و پخش فیلم')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Casual','تفننی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('شبیه‌سازی','شبیه سازی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('کلمات و دانستنی‌ها','کلمات و دانستنی ها')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Simulation','شبیه سازی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Adventure','ماجراجویی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Arcade','آرکید')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Board','تخته‌ای')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Racing','ورزشی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Educational','آموزشی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Role Playing','نقش آفرینی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Strategy','استراتژی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Word','کلمات و دانستنی ها')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Card','کارت بازی')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Casino','کازینو')
+adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('Music','موسیقی')
+# adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('','')
+# adgham_app['categori_name'] = adgham_app['categori_name'].str.replace('','')
+
+
+adgham_app.to_excel(r'D:\python\PowerBI_App_store\progress\test\adgham_app1.xlsx', index=False)
+print('adgham_app :',len(adgham_app))
+adgham_app.to_sql('App_merge',con,if_exists='replace', index=False)
+
+print('اتمام برنامه')
+
+adgham_app.dtypes
+
+creator = adgham_app.query(" creator != ''")
+print('just_creator_v1 :',len(creator))
+creator.to_excel(r'D:\python\PowerBI_App_store\progress\test\creator.xlsx', index=False)
+
+creator.to_sql('creator_App_merge',con,if_exists='replace', index=False)
+
+
+
+cat = adgham_app.query(" cat == 'game'")
+print('just_cat :',len(cat))
+cat.to_excel(r'D:\python\PowerBI_App_store\progress\test\cat.xlsx', index=False)
+cat.to_sql('game_App_merge',con,if_exists='replace', index=False)
+
+
+
+
+learn = adgham_app.query("categori_name == 'آموزشی'")
+print('learn :',len(learn))
+learn.to_excel(r'D:\python\PowerBI_App_store\progress\test\learn.xlsx', index=False)
+learn.to_sql('learn_app',con,if_exists='replace', index=False)
+
+
